b941c1f4ac9689dfa80c6764ecd9d61df343dc53
Luis Espinoza <79593471+luiseel@users.noreply.github.com>
2026-08-07 16:09:03 -0700

    Improve gameplay (#1)
    * improve paddle movement and ball hit behavior
    
    * update hit.wav
    
    * wip
    
    * wip

--- src/entity.c
+++ src/entity.c
@@ -1,31 +1,53 @@
 #include "entity.h"
+#include <stdlib.h>
 
 void update_ball(ball_t *ball, float delta_time) {
     float const speed = 250.f;
 
-    if (ball->position.x < 0.f) {
-        ball->velocity.x = 1.f;
-    }
-    if (ball->position.x + ball->width > GAME_WIDTH) {
-        ball->velocity.x = -1.f;
-    }
-    if (ball->position.y < 0.f) {
-        ball->velocity.y = 1.f;
-    }
-    if (ball->position.y + ball->height > GAME_HEIGHT) {
-        ball->velocity.y = -1.f;
-    }
-
     vector_2d_t velocity = ball->velocity;
-    multiply_vector_by_scalar(&velocity, delta_time * speed); // this is so weird!!
+    multiply_vector_by_scalar(&velocity, delta_time * speed);
     add_vectors(&ball->position, ball->position, velocity);
 }
 
+void reset_ball(ball_t *ball) {
+    ball->position.x = GAME_WIDTH / 2 - ball->width / 2;
+    ball->position.y = GAME_HEIGHT / 2 - ball->height / 2;
+    ball->velocity.x = (rand() % 2 == 0) ? 1.f : -1.f;
+    ball->velocity.y = (rand() % 2 == 0) ? 0.5f : -0.5f;
+}
+
+void check_wall_collisions(ball_t *ball, int *player_one_score, int *player_two_score) {
+    // Top/bottom wall collisions - preserve angle
+    if (ball->position.y < 0.f) {
+        ball->position.y = 0.f;
+        ball->velocity.y = -ball->velocity.y;
+    }
+    if (ball->position.y + ball->height > GAME_HEIGHT) {
+        ball->position.y = GAME_HEIGHT - ball->height;
+        ball->velocity.y = -ball->velocity.y;
+    }
+
+    // Left/right wall collisions - scoring
+    if (ball->position.x < 0.f) {
+        (*player_two_score)++;
+        reset_ball(ball);
+    }
+    if (ball->position.x + ball->width > GAME_WIDTH) {
+        (*player_one_score)++;
+        reset_ball(ball);
+    }
+}
+
 void update_player_one_pad(pad_t *pad, float delta_time) {
-    float const speed = 200.f;
-    vector_2d_t velocity = pad->velocity;
-    multiply_vector_by_scalar(&velocity, delta_time * speed);
-    add_vectors(&pad->position, pad->position, velocity);
+    float const max_speed = 400.f;
+    float const friction = 10.f;
+
+    // Smooth acceleration toward goal velocity
+    pad->velocity.y += (pad->goal_velocity.y - pad->velocity.y) * friction * delta_time;
+
+    // Apply velocity with clamped speed
+    float speed = pad->velocity.y * max_speed;
+    pad->position.y += speed * delta_time;
 
     if (pad->position.y < 0.f) {
         pad->position.y = 0.f;
@@ -35,8 +57,15 @@ void update_player_one_pad(pad_t *pad, float delta_time) {
     }
 }
 
-void update_player_two_pad(pad_t *pad, ball_t *ball, float delta_time) {
-    pad->position.y = ball->position.y - (pad->height / 2.f - ball->height / 2.f);
+void update_player_two_pad(pad_t *pad, float delta_time) {
+    float const max_speed = 400.f;
+    float const friction = 10.f;
+
+    pad->velocity.y += (pad->goal_velocity.y - pad->velocity.y) * friction * delta_time;
+
+    float speed = pad->velocity.y * max_speed;
+    pad->position.y += speed * delta_time;
+
     if (pad->position.y < 0.f) {
         pad->position.y = 0.f;
     }
@@ -66,29 +95,44 @@ void check_collisions(ball_t *ball, pad_t *pad) {
         const float rel_cx = ball_cx - pad_cx;
         const float max_dist = (ball->width + pad->width) / 2.f;
         const float norm_dist = rel_cx / max_dist;
+
+        // Add paddle velocity influence to ball's Y velocity
+        float paddle_influence = pad->velocity.y * 0.5f;
+        float base_y_velocity = ball_yh < pad_y + pad->height / 2.f ? -1.f : 1.f;
+
         ball->velocity.x = norm_dist;
-        ball->velocity.y = ball_yh < pad_y + pad->height / 2.f ? -1.f : 1.f;
+        ball->velocity.y = base_y_velocity + paddle_influence;
         ball->has_collided = true;
     }
 }
 
-void handle_pad_key_press(pad_t *pad, Uint8 const *state) {
-    if (state[SDL_SCANCODE_S] || state[SDL_SCANCODE_W]) {
+void handle_pad_key_press(pad_t *pad, Uint8 const *state, int player_num) {
+    if (player_num == 1) {
         if (state[SDL_SCANCODE_S]) {
-            pad->velocity.y = 1.f;
+            pad->goal_velocity.y = 1.f;
         }
         if (state[SDL_SCANCODE_W]) {
-            pad->velocity.y = -1.f;
+            pad->goal_velocity.y = -1.f;
+        }
+    } else {
+        if (state[SDL_SCANCODE_DOWN]) {
+            pad->goal_velocity.y = 1.f;
+        }
+        if (state[SDL_SCANCODE_UP]) {
+            pad->goal_velocity.y = -1.f;
         }
     }
 }
 
-void handle_pad_key_release(pad_t *pad, Uint8 const *state) {
-    if (!state[SDL_SCANCODE_S] && !state[SDL_SCANCODE_W]) {
-        pad->velocity.y = 0;
-    }
-    if (!state[SDL_SCANCODE_W] && !state[SDL_SCANCODE_S]) {
-        pad->velocity.y = 0;
+void handle_pad_key_release(pad_t *pad, Uint8 const *state, int player_num) {
+    if (player_num == 1) {
+        if (!state[SDL_SCANCODE_S] && !state[SDL_SCANCODE_W]) {
+            pad->goal_velocity.y = 0;
+        }
+    } else {
+        if (!state[SDL_SCANCODE_UP] && !state[SDL_SCANCODE_DOWN]) {
+            pad->goal_velocity.y = 0;
+        }
     }
 }
 
--- src/game.c
+++ src/game.c
@@ -71,16 +71,18 @@ void terminate_game(game_t *game) {
 void handle_game_events(game_t *game) {
     SDL_Event event;
     Uint8 const *state = SDL_GetKeyboardState(NULL);
-    if (SDL_PollEvent(&event)) {
+    while (SDL_PollEvent(&event)) {
         switch (event.type) {
             case SDL_QUIT:
                 game->is_running = FALSE;
                 break;
             case SDL_KEYDOWN:
-                handle_pad_key_press(&game->player_one_pad, state);
+                handle_pad_key_press(&game->player_one_pad, state, 1);
+                handle_pad_key_press(&game->player_two_pad, state, 2);
                 break;
             case SDL_KEYUP:
-                handle_pad_key_release(&game->player_one_pad, state);
+                handle_pad_key_release(&game->player_one_pad, state, 1);
+                handle_pad_key_release(&game->player_two_pad, state, 2);
                 break;
             default:
                 break;
@@ -91,16 +93,10 @@ void handle_game_events(game_t *game) {
 void update_game(game_t *game, float delta_time) {
     update_ball(&game->ball, delta_time);
     update_player_one_pad(&game->player_one_pad, delta_time);
-    update_player_two_pad(&game->player_two_pad, &game->ball, delta_time);
+    update_player_two_pad(&game->player_two_pad, delta_time);
     check_collisions(&game->ball, &game->player_one_pad);
     check_collisions(&game->ball, &game->player_two_pad);
-
-    if (game->ball.position.x < 0) {
-        game->player_two_score++;
-    }
-    if (game->ball.position.x + game->ball.width > GAME_WIDTH) {
-        game->player_one_score++;
-    }
+    check_wall_collisions(&game->ball, &game->player_one_score, &game->player_two_score);
 }
 
 void render_game(game_t *game) {
@@ -110,8 +106,12 @@ void render_game(game_t *game) {
     SDL_SetRenderDrawColor(game->renderer, 39, 39, 39, 0);
     SDL_RenderClear(game->renderer);
 
-    render_game_text(game, GAME_PLAYER_ONE_SCORE_X, GAME_SCORE_Y, player_one_score);
-    render_game_text(game, GAME_PLAYER_TWO_SCORE_X, GAME_SCORE_Y, player_two_score);
+    if (player_one_score != NULL) {
+        render_game_text(game, GAME_PLAYER_ONE_SCORE_X, GAME_SCORE_Y, player_one_score);
+    }
+    if (player_two_score != NULL) {
+        render_game_text(game, GAME_PLAYER_TWO_SCORE_X, GAME_SCORE_Y, player_two_score);
+    }
 
     render_entity(&game->ball, game->renderer);
     render_entity(&game->player_one_pad, game->renderer);
@@ -146,8 +146,8 @@ int run_game(game_t *game) {
         current_time = SDL_GetTicks();
         delta_time = (current_time - last_time) / 1000.0f;
 
-        if (delta_time > target_frame_time) {
-            delta_time = target_frame_time;
+        if (delta_time > target_frame_time / 1000.0f) {
+            delta_time = target_frame_time / 1000.0f;
         }
 
         handle_game_events(game);
@@ -191,7 +191,14 @@ void render_game_text(game_t *game, float x, float y, char *text) {
     int font_height = 0;
 
     SDL_Surface *surface = TTF_RenderText_Solid(game->font, text, base_color);
+    if (surface == NULL) {
+        return;
+    }
     SDL_Texture *texture = SDL_CreateTextureFromSurface(game->renderer, surface);
+    if (texture == NULL) {
+        SDL_FreeSurface(surface);
+        return;
+    }
     SDL_QueryTexture(texture, NULL, NULL, &font_width, &font_height);
     SDL_Rect dest = { x, y, font_width, font_height };
 
